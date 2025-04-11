efe8d78493c4c01fadf8c1ea4314922b26f06c4d
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-04-11 05:55:04 +0530

    Remove unnecessary context providers from App component for simplified rendering
    - Eliminated ServicesProvider and ThemeProvider from the App component, streamlining the rendering process.
    - Improved code clarity by reducing complexity in the component structure, focusing on the core application functionality.

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -2,15 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { ThemeProvider } from "@/components/theme/context/ThemeProvider.tsx";
-import { ServicesProvider } from "./context/services/ServiceProvider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ServicesProvider>
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
-    </ServicesProvider>
+    <App />
   </StrictMode>
 );
