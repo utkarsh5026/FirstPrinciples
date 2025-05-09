914db8776de19e9a94c6d5f3232f7e0a11ee3487
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-05-09 18:58:13 +0530

    Remove ReadingTimeTracker component to streamline document reading utilities
    - Deleted the ReadingTimeTracker component, which was previously used for tracking reading sessions in the background.
    - Updated the export list in the utils index file to reflect this removal, enhancing code organization and clarity.
    - This change aims to simplify the document reading utilities and improve maintainability.

--- app/src/components/document-reading/preview/utils/index.ts
+++ app/src/components/document-reading/preview/utils/index.ts
@@ -1,13 +1,11 @@
 import LoadingScreen from "./LoadingScreen";
 import ErrorLoadingDocument from "./ErrorLoadingDocument";
 import NoFileSelectedYet from "./NoFileSelectedYet";
-import ReadingTimeTracker from "./ReadingTimeTracker";
 import ReadingSessionDialog from "./ReadingSessionDialog";
 
 export {
   LoadingScreen,
   ErrorLoadingDocument,
   NoFileSelectedYet,
-  ReadingTimeTracker,
   ReadingSessionDialog,
 };
