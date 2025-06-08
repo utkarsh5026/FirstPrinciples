975eb55b93e38e83b8698ff9d02b33dc137e16e7
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-06-08 23:20:01 +0530

    Refactor data structure icon mappings to streamline imports and reduce redundancy
    - Removed unused icons and their associated mappings, including TreePine, Box, and several others.
    - Simplified the keywords for certain icons to enhance clarity and relevance.
    - Updated imports to include only necessary icons, improving code maintainability and readability.

--- app/src/components/shared/icons/mappings/dataStructureMappings.tsx
+++ app/src/components/shared/icons/mappings/dataStructureMappings.tsx
@@ -1,41 +1,25 @@
 import {
   Binary,
-  TreePine,
   Triangle,
   Grid3x3,
   GitCommit,
   Users,
   Hash,
-  Box,
   Network,
-  Merge,
   Calculator,
-  TrendingUp,
   Target,
-  Zap,
   Split,
-  ArrowUpDown,
   Search,
-  ChevronDown,
-  Layers,
   Route,
   RotateCcw,
-  FileText,
-  Shuffle,
   Minimize2,
   Maximize2,
-  Brain,
+  Move,
   LucideProps,
 } from "lucide-react";
-import {
-  FaProjectDiagram,
-  FaSort,
-  FaSortAmountDown,
-  FaRoute,
-} from "react-icons/fa";
-import { BsTree, BsGrid, BsGraphUp } from "react-icons/bs";
+import { FaSortAmountDown } from "react-icons/fa";
+import { BsTree, BsGrid } from "react-icons/bs";
 import { GiStack } from "react-icons/gi";
-import { AiOutlinePartition, AiOutlineNodeIndex } from "react-icons/ai";
 import { TbNavigationSearch } from "react-icons/tb";
 import { IconMapping } from "./types";
 
@@ -47,10 +31,6 @@ export const dataStructureMappings: IconMapping[] = [
   {
     keywords: ["binary tree", "btree", "b-tree"],
     icon: (props: LucideProps) => <Binary {...props} />,
-  },
-  {
-    keywords: ["red black tree", "red-black"],
-    icon: (props: LucideProps) => <TreePine {...props} />,
   },
   {
     keywords: ["binary search trees"],
@@ -83,7 +63,7 @@ export const dataStructureMappings: IconMapping[] = [
     isReactIcon: true,
   },
   {
-    keywords: ["queue", "fifo", "circular queue", "deque"],
+    keywords: ["queue"],
     icon: (props: LucideProps) => <Users {...props} />,
   },
 
@@ -91,19 +71,10 @@ export const dataStructureMappings: IconMapping[] = [
     keywords: ["hash table", "hash map", "dictionary"],
     icon: (props: LucideProps) => <Hash {...props} />,
   },
-  {
-    keywords: ["set", "hash set"],
-    icon: (props: LucideProps) => <Box {...props} />,
-  },
 
   {
     keywords: ["graph", "directed graph", "undirected graph"],
     icon: (props: LucideProps) => <Network {...props} />,
-  },
-  {
-    keywords: ["adjacency list", "adjacency matrix"],
-    icon: (props: LucideProps) => <FaProjectDiagram {...props} />,
-    isReactIcon: true,
   },
 
   {
@@ -112,33 +83,14 @@ export const dataStructureMappings: IconMapping[] = [
     isReactIcon: true,
   },
 
-  // Advanced Structures
-  {
-    keywords: ["segment tree", "fenwick tree", "bit"],
-    icon: (props: LucideProps) => <AiOutlinePartition {...props} />,
-    isReactIcon: true,
-  },
-  {
-    keywords: ["union find", "disjoint set"],
-    icon: (props: LucideProps) => <Merge {...props} />,
-  },
-
   {
     keywords: ["dynamic programming", "dp", "memoization"],
     icon: (props: LucideProps) => <Calculator {...props} />,
-  },
-  {
-    keywords: ["knapsack", "longest common subsequence", "lcs"],
-    icon: (props: LucideProps) => <TrendingUp {...props} />,
   },
 
   {
     keywords: ["greedy", "greedy algorithm"],
     icon: (props: LucideProps) => <Target {...props} />,
-  },
-  {
-    keywords: ["activity selection", "huffman coding"],
-    icon: (props: LucideProps) => <Zap {...props} />,
   },
 
   // Divide and Conquer
@@ -147,16 +99,6 @@ export const dataStructureMappings: IconMapping[] = [
     icon: (props: LucideProps) => <Split {...props} />,
   },
 
-  // Sorting Algorithms
-  {
-    keywords: ["bubble sort", "selection sort", "insertion sort"],
-    icon: (props: LucideProps) => <ArrowUpDown {...props} />,
-  },
-  {
-    keywords: ["heap sort", "radix sort", "counting sort"],
-    icon: (props: LucideProps) => <FaSort {...props} />,
-    isReactIcon: true,
-  },
   {
     keywords: ["sort", "sorting"],
     icon: (props: LucideProps) => <FaSortAmountDown {...props} />,
@@ -168,50 +110,11 @@ export const dataStructureMappings: IconMapping[] = [
     keywords: ["binary search", "linear search"],
     icon: (props: LucideProps) => <Search {...props} />,
   },
-  {
-    keywords: ["depth first search", "dfs"],
-    icon: (props: LucideProps) => <ChevronDown {...props} />,
-  },
-  {
-    keywords: ["breadth first search", "bfs"],
-    icon: (props: LucideProps) => <Layers {...props} />,
-  },
-
-  // Graph Algorithms
-  {
-    keywords: ["dijkstra", "shortest path"],
-    icon: (props: LucideProps) => <Route {...props} />,
-  },
-  {
-    keywords: ["bellman ford", "floyd warshall"],
-    icon: (props: LucideProps) => <FaRoute {...props} />,
-    isReactIcon: true,
-  },
-  {
-    keywords: ["minimum spanning tree", "mst", "kruskal", "prim"],
-    icon: (props: LucideProps) => <BsGraphUp {...props} />,
-    isReactIcon: true,
-  },
-  {
-    keywords: ["topological sort", "cycle detection"],
-    icon: (props: LucideProps) => <AiOutlineNodeIndex {...props} />,
-    isReactIcon: true,
-  },
 
   // Backtracking
   {
     keywords: ["backtracking", "n queens", "sudoku"],
     icon: (props: LucideProps) => <RotateCcw {...props} />,
-  },
-
-  // String Algorithms
-  {
-    keywords: ["string matching", "kmp", "rabin karp"],
-    icon: (props: LucideProps) => <FileText {...props} />,
-  },
-  {
-    keywords: ["edit distance", "levenshtein"],
-    icon: (props: LucideProps) => <Shuffle {...props} />,
   },
 
   // Recursion
@@ -220,31 +123,20 @@ export const dataStructureMappings: IconMapping[] = [
     icon: (props: LucideProps) => <RotateCcw {...props} />,
   },
 
-  // Mathematical Algorithms
-  {
-    keywords: ["prime", "gcd", "lcm", "modular arithmetic"],
-    icon: (props: LucideProps) => <Calculator {...props} />,
-  },
-  {
-    keywords: ["fibonacci", "factorial"],
-    icon: (props: LucideProps) => <TrendingUp {...props} />,
-  },
-
-  // Optimization
   {
     keywords: ["optimization", "minimize", "maximize"],
     icon: (props: LucideProps) => <Minimize2 {...props} />,
   },
   {
-    keywords: ["two pointer", "sliding window"],
+    keywords: ["two pointer"],
     icon: (props: LucideProps) => <Maximize2 {...props} />,
   },
 
-  // Advanced Algorithms
   {
-    keywords: ["machine learning", "ai algorithm"],
-    icon: (props: LucideProps) => <Brain {...props} />,
+    keywords: ["sliding window"],
+    icon: (props: LucideProps) => <Move {...props} />,
   },
+
   {
     keywords: ["bit manipulation", "bitwise"],
     icon: (props: LucideProps) => <Binary {...props} />,
