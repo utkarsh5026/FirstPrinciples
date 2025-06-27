8438a5c1b8a02206d5889198f6aa6e6a17ae8268
Utkarsh Priyadarshi <utkarshpriyadarshi5026@gmail.com>
2025-06-27 19:39:32 +0530

    Add Debugging Log for Time Spent Metrics in StatsCards Component
    - Added a console log to output today's time spent and total time spent for debugging purposes.
    - This change aims to assist in monitoring and analyzing user reading patterns effectively.

--- app/src/components/home/overview/components/stats-cards.tsx
+++ app/src/components/home/overview/components/stats-cards.tsx
@@ -39,6 +39,8 @@ const StatsCards: React.FC<StatsCardsProps> = ({ nextMilestone }) => {
     [history]
   );
 
+  console.log(todayTimeSpent, totalTimeSpent);
+
   const containerVariants = {
     hidden: { opacity: 0 },
     visible: {
